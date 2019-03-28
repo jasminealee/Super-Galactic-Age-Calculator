cb5953dc7e451c9845257c845e71f0c57bb36601
Jasmine Lee <jasmine.al1722@gmail.com>
2019-03-27 18:41:55 -0700

    add month array and variables for date, month, and year inputs; set var for life expectancy to 80 and variable and information for adjustedbirthyear

--- src/Birthday.js
+++ src/Birthday.js
@@ -6,43 +6,8 @@ $(document).ready(function() {
     var d1 = parseInt($("#birthDate").val());
     var m1 = parseInt($("#birthMonth").val());
     var y1 = parseInt($("#birthYear").val());
-
+    var l1 = 80;
 
     var adjustedBirthYear = y1 + 1;
-
-    var adjustedBirthYearDays = function() {
-      var days = 0;
-      for (var i = (m1 - 1); i < month.length; i++) {
-        days += month[i];
-      }
-      return days - d1;
-    }
-
-    var date = new Date();
-    var currentYear = date.getFullYear();
-
-    var currentYearDays = function() {
-      var currentMonth = date.getMonth();
-      var currentDay = date.getDate();
-      var days = 0;
-      for (var i = 0; i < currentMonth; i++) {
-        days += month[i];
-      }
-      return days + currentDay;
-    }
-
-    var yCounter = function() {
-      var ageYears = currentYear - adjustedBirthYear;
-      var addDays = currentYearDays() + adjustedBirthYearDays();
-      var partialYear = addDays / 365;
-      if (d1 > month[m1 - 1] || d1 <= 0 || m1 > month.length || m1 <= 0) {
-        return "Date not real";
-      }
-      else {
-        return ageYears + partialYear;
-      }
-    }
-
-    $("#result").text(yCounter());
   });
 });
